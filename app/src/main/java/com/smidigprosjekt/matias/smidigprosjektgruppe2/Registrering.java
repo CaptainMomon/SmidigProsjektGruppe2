0ca1f264eede049a9a29da5042b0e5228ff7076a
CaptainMomo <31258188+CaptainMomo@users.noreply.github.com>
2018-12-19 13:19:58 +0100

    Initial commit

---
+++ app/src/main/java/com/smidigprosjekt/matias/smidigprosjektgruppe2/Registrering.java
@@ -0,0 +1,26 @@
+package com.smidigprosjekt.matias.smidigprosjektgruppe2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Registrering extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrering);
+
+        Button registerBtn = (Button)findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+}

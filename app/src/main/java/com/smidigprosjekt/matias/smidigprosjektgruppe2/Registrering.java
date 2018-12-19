9a1aa52ebb2d0398e0cfbb0c9cc7f0b15448606c
CaptainMomo <31258188+CaptainMomo@users.noreply.github.com>
2018-12-19 14:57:11 +0100

    Homepage og Registrering med facebook

--- app/src/main/java/com/smidigprosjekt/matias/smidigprosjektgruppe2/Registrering.java
+++ app/src/main/java/com/smidigprosjekt/matias/smidigprosjektgruppe2/Registrering.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+
 
 public class Registrering extends AppCompatActivity {
 

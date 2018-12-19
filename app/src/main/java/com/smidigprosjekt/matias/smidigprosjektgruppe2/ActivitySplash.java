89adec44540387881752bf9321602be2d2771de4
CaptainMomon <dalmat17@student.westerdals.no>
2018-12-19 21:44:22 +0100

    + en Intro side med logo

---
+++ app/src/main/java/com/smidigprosjekt/matias/smidigprosjektgruppe2/ActivitySplash.java
@@ -0,0 +1,36 @@
+package com.smidigprosjekt.matias.smidigprosjektgruppe2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.smidigprosjekt.matias.smidigprosjektgruppe2.LoginActivity;
+import com.smidigprosjekt.matias.smidigprosjektgruppe2.R;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 3000; //This is 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Code to start timer and take action after the timer ends
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(ActivitySplash.this, LoginActivity.class);
+                startActivity(mySuperIntent);
+                /* This 'finish()' is for exiting the app when back button pressed
+                *  from Home page which is ActivityHome
+                */
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
